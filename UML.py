60b45016f4bf8e1a76db6ea19416f3a2101ab415
Mariana Granados <mgranados09@alumnos.uaq.mx>
2022-10-27 00:54:19 -0500

    notes on UML diagrams: the class diagram

---
+++ UML.py
@@ -0,0 +1,14 @@
+
+#UML (Unified Modeling Language) diagrams are used to express 
+#the organization of a program. UML diagrams are a standard visual notation
+#to express object-oriented software designs.
+
+#One type of UML figure is known as a class diagram 
+#the diagram has three portions, first designating the name of the class
+#the second the recommneded instance variables and the third the recommended methods of the class
+
+#Class: CreditCard
+
+#Fields:    _customer, _bank, _account
+
+#Behaviors: get_customer(), get_bank(), get_account() ...
